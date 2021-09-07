6ccb6945c707c90839890fd1ba59168023b2d9a4
Klemen Pavlovič <klemenpavlovic@gmail.com>
2021-09-07 15:17:42 +0200

    ViewInvoice mobile styling finished

--- src/components/ViewInvoice.js
+++ src/components/ViewInvoice.js
@@ -21,6 +21,17 @@ export default function ViewInvoice() {
     statusColor = "#33d69f";
   }
 
+  let receiptItems = state.items.map((item) => (
+    <StyledReceiptItemsDiv>
+      <div>
+        <p className="black">{item.name}</p>
+        <p>{`${item.quantity} x € ${item.price}`}</p>
+      </div>
+      <p className="black">{`€ ${item.total}`}</p>
+    </StyledReceiptItemsDiv>
+  ));
+  console.log(receiptItems);
+
   return (
     <StyledViewInvoiceDiv>
       <StyledBackLink to="/">
@@ -37,17 +48,48 @@ export default function ViewInvoice() {
         </StyledStatusStateDiv>
       </StyledStatusDiv>
       <StyledInvoiceInfoDiv>
-        <p>
-          <span>#</span>
-          {state.id}
-        </p>
-        <div>address</div>
-        <div>
-          <div>title</div>
-          <div>title</div>
-        </div>
-        <div>title</div>
-        <div>title</div>
+        <StyledDivId>
+          <p>
+            <span>#</span>
+            {state.id}
+          </p>
+          <p>{state.description}</p>
+        </StyledDivId>
+        <StyledSenderAddressDiv>
+          <p>{state.senderAddress.street}</p>
+          <p>{state.senderAddress.city}</p>
+          <p>{state.senderAddress.postCode}</p>
+          <p>{state.senderAddress.country}</p>
+        </StyledSenderAddressDiv>
+        <StyledDateClientDiv>
+          <StyledDateDiv>
+            <p>Invoice Date</p>
+            <p className="bold">{state.createdAt}</p>
+            <p>Payment Due</p>
+            <p className="bold">{state.paymentDue}</p>
+          </StyledDateDiv>
+          <StyledBillToDiv>
+            <p>Bill To</p>
+            <p className="bold">{state.clientName}</p>
+            <p>{state.clientAddress.street}</p>
+            <p>{state.clientAddress.city}</p>
+            <p>{state.clientAddress.postCode}</p>
+            <p>{state.clientAddress.country}</p>
+          </StyledBillToDiv>
+        </StyledDateClientDiv>
+        <StyledSentToDiv>
+          <p>Sent to</p>
+          <p>{state.clientEmail}</p>
+        </StyledSentToDiv>
+        <StyledReceiptDiv>
+          <StyledRecepitItemsContainerDiv>
+            {receiptItems}
+          </StyledRecepitItemsContainerDiv>
+          <StyledGrandTotalContainer>
+            <p>Grand Total</p>
+            <p>{`€ ${state.total}`}</p>
+          </StyledGrandTotalContainer>
+        </StyledReceiptDiv>
       </StyledInvoiceInfoDiv>
     </StyledViewInvoiceDiv>
   );
@@ -114,4 +156,128 @@ const StyledInvoiceInfoDiv = styled.div`
   background: #fff;
   border-radius: 10px;
   padding: 24px;
+  color: var(--text-light-form);
+`;
+
+const StyledDivId = styled.div`
+  font-weight: bold;
+  color: var(--text-light-black);
+
+  span {
+    color: var(--text-light-form);
+  }
+
+  p:nth-child(1) {
+    margin: 0;
+    margin-bottom: 4px;
+  }
+
+  p:nth-child(2) {
+    color: var(--text-light-form);
+    font-weight: normal;
+    margin: 0;
+    //margin-bottom: 30px;
+  }
+`;
+
+const StyledSenderAddressDiv = styled.div`
+  margin: 30px 0px;
+
+  p {
+    margin: 0;
+    line-height: 18px;
+    font-size: 11px;
+  }
+`;
+
+const StyledDateClientDiv = styled.div`
+  display: grid;
+  //flex-direction: row;
+  //justify-content: space-between;
+  grid-template: 1fr / 1fr 1fr;
+  grid-gap: 41px;
+
+  .bold {
+    font-size: 15px;
+    line-height: 20px;
+    letter-spacing: -0.31px;
+    font-weight: bold;
+    color: var(--text-light-black);
+  }
+`;
+
+const StyledDateDiv = styled.div`
+  p:nth-child(3) {
+    margin-top: 32px;
+  }
+`;
+
+const StyledBillToDiv = styled.div`
+  p:nth-child(3),
+  p:nth-child(4),
+  p:nth-child(5),
+  p:nth-child(6) {
+    margin: 0;
+    line-height: 1.5;
+    font-size: 11px;
+  }
+`;
+
+const StyledSentToDiv = styled.div`
+  margin-top: 32px;
+
+  p:nth-child(1) {
+    margin: 0;
+  }
+
+  p:nth-child(2) {
+    font-weight: bold;
+    color: var(--text-light-black);
+    font-size: 15px;
+    line-height: 20px;
+    letter-spacing: -0.31px;
+    margin: 12px 0px 0px 0px;
+  }
+`;
+
+const StyledReceiptDiv = styled.div`
+  margin-top: 40px;
+  //padding: 24px;
+`;
+
+const StyledRecepitItemsContainerDiv = styled.div`
+  background: #f9fafe;
+  border-radius: 10px 10px 0px 0px;
+  padding: 12px 24px;
+`;
+
+const StyledReceiptItemsDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  font-weight: bold;
+
+  .black {
+    color: var(--text-light-black);
+  }
+`;
+
+const StyledGrandTotalContainer = styled.div`
+  background: #373b53;
+  border-radius: 0px 0px 10px 10px;
+  padding: 0px 24px;
+  color: #fff;
+  font-size: 11px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  p:nth-child(2) {
+    font-weight: bold;
+    font-size: 20px;
+    line-height: 32px;
+    letter-spacing: -0.42px;
+  }
 `;
